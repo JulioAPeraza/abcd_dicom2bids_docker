fa385aa76801bce4b4136c63157d71ca49d2dff6
Michael Riedel <miriedel@fiu.edu>
2021-07-09 09:17:59 -0400

    fixed modalities

--- abcd_gen_dockerfile.sh
+++ abcd_gen_dockerfile.sh
@@ -23,14 +23,14 @@ neurodocker generate docker \
             --dcm2niix version=latest method=source \
             --matlabmcr version=2018a method=binaries \
             --fsl version=5.0.10 method=binaries \
-            --workdir=/opt/abcd_dicom2bids_docker \
-            --run-bash 'git clone https://github.com/NBCLab/abcd_dicom2bids_docker.git /opt/abcd_dicom2bids_dockerimg2' \
             --run-bash 'mkdir /work/; mkdir /data/; mkdir /out/; mkdir ~/.aws/' \
             --add-to-entrypoint "source activate /opt/miniconda-latest/envs/neuro" \
             --workdir=/opt/docker \
             --run 'curl -sSL -o /opt/docker/docker-ce-cli_19.03.15~3-0~debian-stretch_amd64.deb https://download.docker.com/linux/debian/dists/stretch/pool/stable/amd64/docker-ce-cli_19.03.15~3-0~debian-stretch_amd64.deb' \
             --run 'dpkg -i /opt/docker/docker-ce-cli_19.03.15~3-0~debian-stretch_amd64.deb' \
-            --entrypoint "/neurodocker/startup.sh python3 /opt/abcd_dicom2bids_dockerimg2/entrypoint.py"
+            --workdir=/opt/abcd_dicom2bids_docker \
+            --run-bash 'git clone https://github.com/NBCLab/abcd_dicom2bids_docker.git /opt/abcd_dicom2bids_dockerimg3' \
+            --entrypoint "/neurodocker/startup.sh python3 /opt/abcd_dicom2bids_dockerimg3/entrypoint.py"
 
 #build docker image
 docker build -t abcddicom2bids - < /Users/miriedel/Desktop/GitHub/abcd_dicom2bids_docker/dockerfile_07092021
--- entrypoint.py
+++ entrypoint.py
@@ -7,8 +7,8 @@ def get_parser():
     parser = argparse.ArgumentParser(description='Downloads ABCD participant'
              'data and converts to BIDS format')
     parser.add_argument('--subjects', required=True, dest='subs', nargs='+')
-    parser.add_argument('--modalities', required=False, dest='modalities', default=['anat', 'func'])
-    parser.add_argument('--sessions', required=False, dest='sessions', default=['baseline_year_1_arm_1', '2_year_follow_up_y_arm_1'])
+    parser.add_argument('--modalities', required=False, dest='modalities', nargs='+', default="'anat', 'func'")
+    parser.add_argument('--sessions', required=False, dest='sessions', nargs='+', default=['baseline_year_1_arm_1', '2_year_follow_up_y_arm_1'])
     return parser
 
 
@@ -27,13 +27,14 @@ def main(argv=None):
 
     cmd="python3 /opt/abcd-dicom2bids/abcd2bids.py /opt/fsl-5.0.10/ /opt/matlabmcr-2018a/v94/ \
       --subject-list {subject_text_file} \
-      --modalities '{modalities}' \
+      --modalities {modalities} \
       --sessions {sessions} \
+      --temp /work \
       --qc /data/qc_spreadsheet.txt \
       --output /out/ \
       --config /data/config_file.ini \
       --remove".format(subject_text_file=subject_text_file,
-                       modalities="' '".join(args.modalities),
+                       modalities=args.modalities,
                        sessions=args.sessions)
 
     os.system(cmd)
